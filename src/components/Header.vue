c5ad8fec5c8dd3bee034a70ce919b11053f64e56
rdvdev2 <me@rdvdev2.com>
2023-07-21 19:18:10 +0200

    A bit more branding

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,13 @@
 <template>
-  <header class="bg-purple-900 p-4 flex justify-center drop-shadow-xl">
-    <h1 class="font-bold text-2xl">Nara DEV</h1>
+  <header class="bg-purple-900 p-4 flex justify-between items-center drop-shadow-xl">
+    <div class="flex gap-5">
+      <img class="h-8 rounded" src="/profile.png" alt="Profile image" />
+      <h1 class="font-bold text-2xl">Nara DEV</h1>
+    </div>
+    <div class="flex">
+      <p>Missing links</p>
+    </div>
   </header>
 </template>
+<script setup lang="ts">
+</script>
