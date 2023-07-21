bfbf8dc8791eed5fdb0dd70fd945944101f94ed6
rdvdev2 <me@rdvdev2.com>
2023-07-21 19:08:06 +0200

    Add AboutMe

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 import InConstructionBanner from "./components/WIPBanner.vue";
+import AboutMe from "./components/AboutMe.vue";
 </script>
 
 <template>
-  <div class="h-screen flex flex-col bg-gray-800 text-white">
+  <div class="min-h-screen flex flex-col bg-gray-800 text-white">
     <InConstructionBanner />
     <Header />
-    <main class="flex-grow">
+    <main class="flex-grow flex flex-col p-5 items-center">
+      <AboutMe />
     </main>
     <Footer />
   </div>
---
+++ src/components/AboutMe.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-600 p-4 rounded-2xl flex gap-4 flex-wrap justify-center items-center w-fit">
+    <img class="rounded-xl max-w-sm my-auto mx-0" src="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt="Placeholder image of a computer">
+    <p class="text-justify max-w-prose">
+      My interest in coding started when I was 10 years old, and I needed to learn <b>Lua</b> to play with the Minecraft
+      Mod ComputerCraft. Since then, my curiosity has led me to explore and learn everything about programming that I
+      can find and understand on the Internet. Although I have explored technologies such as <b>C++</b>, <b>OpenGL</b>,
+      <b>Vulkan</b>, <b>HTML</b> or <b>Python</b>, I feel really comfortable with <b>Java</b> and <b>Rust</b>, and I try
+      to develop my projects on those platforms. If I'm known for something, it is for my Minecraft mods, and thus I try
+      to put a big effort on them and create quality content for the players. On this web page, you will find all sort
+      of <b>projects</b> and <b>devlogs</b> written by me. I hope you enjoy it, and maybe find some piece of wisdom that
+      helps you on your current or future projects!
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
