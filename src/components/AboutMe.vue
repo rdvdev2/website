3f4c87a5c89555a33341e54450d7acc1349ac10a
rdvdev2 <me@rdvdev2.com>
2023-07-21 19:42:26 +0200

    Improve readability

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -7,7 +7,9 @@
       can find and understand on the Internet. Although I have explored technologies such as <b>C++</b>, <b>OpenGL</b>,
       <b>Vulkan</b>, <b>HTML</b> or <b>Python</b>, I feel really comfortable with <b>Java</b> and <b>Rust</b>, and I try
       to develop my projects on those platforms. If I'm known for something, it is for my Minecraft mods, and thus I try
-      to put a big effort on them and create quality content for the players. On this web page, you will find all sort
+      to put a big effort on them and create quality content for the players.
+      <br><br>
+      On this web page, you will find all sort
       of <b>projects</b> and <b>devlogs</b> written by me. I hope you enjoy it, and maybe find some piece of wisdom that
       helps you on your current or future projects!
     </p>
