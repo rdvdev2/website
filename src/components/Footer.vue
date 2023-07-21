847d55dde83c2515520b0ab0d5788282e79bf83b
rdvdev2 <me@rdvdev2.com>
2023-07-21 19:19:04 +0200

    rdvdev2 is no more

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="bg-gray-700 flex justify-center p-1 text-sm shadow-xl">
-    © 2023, Nara "rdvdev2" Díaz Viñolas. All rights reserved.
+    © 2023, Nara Díaz Viñolas. All rights reserved.
   </footer>
 </template>
