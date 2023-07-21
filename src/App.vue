1c11c8c972401b3019d890298f544fe5dd9f5b14
rdvdev2 <me@rdvdev2.com>
2023-07-21 18:28:53 +0200

    Move WIP warning to header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
-import InConstruction from "./components/InConstruction.vue";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import InConstructionBanner from "./components/WIPBanner.vue";
 </script>
 
 <template>
   <div class="h-screen flex flex-col bg-gray-800 text-white">
+    <InConstructionBanner />
     <Header />
     <main class="flex-grow">
-      <InConstruction />
     </main>
     <Footer />
   </div>
---
+++ src/components/WIPBanner.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex justify-center bg-yellow-500 p-1.5">
+    <h1 class="text-black italic text-sm">🚧 This website is a Work In Progress! 🚧</h1>
+  </div>
+</template>
