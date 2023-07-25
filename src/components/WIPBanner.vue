786a283d9486fa497c54a561d02036352b155223
Nara Díaz Viñolas <me@rdvdev2.com>
2023-07-25 02:58:03 +0200

    Allow hiding of the WIP banner

--- src/components/WIPBanner.vue
+++ src/components/WIPBanner.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import {reactive, ref} from "vue"
+
+const hidden = ref(false);
+
+const bannerStyle = reactive({
+  'h-0': hidden,
+  'p-0': hidden,
+})
+
+function hide() {
+  hidden.value = true;
+}
+</script>
+
 <template>
-  <div class="flex justify-center bg-yellow-500 p-1.5">
+  <div class="flex flex-row justify-center bg-yellow-500 p-1.5 transition-all gap-1" :class="bannerStyle">
     <h1 class="text-black italic text-sm">🚧 This website is a Work In Progress! 🚧</h1>
+    <button class="underline text-black text-xs" type="button" @click="hide">Hide</button>
   </div>
 </template>
