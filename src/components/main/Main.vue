555d92366d33d611e14d8aedf196af0481777f45
rdvdev2 <me@rdvdev2.com>
2023-07-29 15:26:56 +0200

    Add featured projects

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Hello from "./Hello.vue";
 import AboutMe from "./AboutMe.vue";
+import Projects from "./Projects.vue";
 </script>
 
 <template>
   <main class="flex-grow flex flex-col p-5 items-center gap-5">
     <Hello />
     <AboutMe />
+    <Projects />
   </main>
 </template>
---
+++ src/components/main/Project.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {computed} from "vue";
+
+const props = defineProps({
+  'bgUrl': String,
+  'href': String,
+})
+
+const background = computed(() => `url('${props.bgUrl}')`)
+
+function openLink() {
+  open(props.href, "_self");
+}
+</script>
+
+<template>
+  <div @click="openLink" class="bg-black overflow-hidden aspect-square max-w-xs rounded-3xl border-purple-500 border-2 transition-all hover:scale-105 cursor-pointer">
+    <div id="inner" class="flex flex-col justify-center items-center p-4 w-full h-full transition-all opacity-70 hover:opacity-100">
+      <h1 class="text-xl font-bold"><slot name="name"></slot></h1>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#inner {
+  --background: rgba(0, 0, 0, 0.6);
+  background: linear-gradient(var(--background), var(--background)), v-bind('background') center / cover;
+}
+</style>
