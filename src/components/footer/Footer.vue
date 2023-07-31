9b38eca9d722aa7511c3b337f2924e8d0ed1d2e7
Nara Díaz Viñolas <me@rdvdev2.com>
2023-07-31 15:30:50 +0200

    Fix production env

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
 import Social from "./Social.vue";
+
+import githubLogo from '/public/socials/github.svg'
+import modrinthLogo from '/public/socials/modrinth.svg'
+import curseforgeLogo from '/public/socials/curseforge.svg'
+import kofiLogo from '/public/socials/kofi.svg'
 </script>
 
 <template>
   <footer class="bg-gray-700 flex flex-col justify-center items-center p-1 text-sm shadow-xl">
     <div class="flex my-2 gap-4">
-      <Social href="https://github.com/rdvdev2" img-src="/socials/github.svg" img-alt="GitHub image link"/>
-      <Social href="https://modrinth.com/user/rdvdev2" img-src="/socials/modrinth.svg" img-alt="Modrinth image link"/>
-      <Social href="https://curseforge.com/members/rdvdev2" img-src="/socials/curseforge.svg" img-alt="Curseforge image link"/>
-      <Social href="https://ko-fi.com/rdvdev2" img-src="/socials/kofi.svg" img-alt="Ko-Fi image link"/>
+      <Social href="https://github.com/rdvdev2" :img-src="githubLogo" img-alt="GitHub image link"/>
+      <Social href="https://modrinth.com/user/rdvdev2" :img-src="modrinthLogo" img-alt="Modrinth image link"/>
+      <Social href="https://curseforge.com/members/rdvdev2" :img-src="curseforgeLogo" img-alt="Curseforge image link"/>
+      <Social href="https://ko-fi.com/rdvdev2" :img-src="kofiLogo" img-alt="Ko-Fi image link"/>
     </div>
     © 2023, Nara Díaz Viñolas. All rights reserved.
   </footer>
