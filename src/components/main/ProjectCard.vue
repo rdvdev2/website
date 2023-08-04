bd5d87c54de29a54e6ea2ddfba628594b108280b
Nara Díaz Viñolas <me@rdvdev2.com>
2023-08-04 15:01:35 +0200

    Justify project card descriptions

--- src/components/main/ProjectCard.vue
+++ src/components/main/ProjectCard.vue
@@ -15,7 +15,7 @@ function openLink() {
   <div @click="openLink" class="bg-black overflow-hidden aspect-square max-w-xs rounded-3xl border-purple-500 border-2 transition-all hover:scale-105 cursor-pointer">
     <div id="inner" class="grid grid-cols-1 grid-rows-2 items-center p-4 w-full h-full transition-all opacity-70 hover:opacity-100">
       <h1 class="text-xl font-bold text-center">{{ project.name }}</h1>
-      <p>{{ project.short_description }}</p>
+      <p class="text-justify">{{ project.short_description }}</p>
     </div>
   </div>
 </template>
